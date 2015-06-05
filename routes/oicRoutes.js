d0164ee528fc7326f65c9ba010f36a32c4a5f7f3
Sakari Poussa <sakari.poussa@intel.com>
2015-06-05 13:18:45 +0300

    Make /oic/res, /oic/p, and /oic/d APIs to work

--- routes/oicRoutes.js
+++ routes/oicRoutes.js
@@ -6,38 +6,65 @@ var routes = function(iotivity) {
 
   router.route('/p')
     .get(function(req, res) {
-      var platform = {"p": "platform"};
-      res.json(platform);
-    });
-
-  router.route('/d')
-    .get(function(req, res) {
-      var devices = {"d": "devices"};
-      res.json(devices);
-    });
-
-  router.route('/res')
-    .get(function(req, res) {
-      var r = {"res": "resources"};
       var handle = {};
 
       iotivity.OCDoResource(
         handle,
         iotivity.OCMethod.OC_REST_GET,
-        "light",
+        "/oic/p",
         null,
         null,
         iotivity.OCConnectivityType.OC_ALL,
         iotivity.OCQualityOfService.OC_HIGH_QOS,
         function( handle, response ) {
-          console.log( "OCDoResource() handler: Entering" );
+          console.log( response );
           res.json(response);
           return iotivity.OCStackApplicationResult.OC_STACK_DELETE_TRANSACTION;
         },
         null,
         0 );
+    });
 
-      res.json(r);
+  router.route('/d')
+    .get(function(req, res) {
+      var handle = {};
+
+      iotivity.OCDoResource(
+        handle,
+        iotivity.OCMethod.OC_REST_GET,
+        "/oic/d",
+        null,
+        null,
+        iotivity.OCConnectivityType.OC_ALL,
+        iotivity.OCQualityOfService.OC_HIGH_QOS,
+        function( handle, response ) {
+          console.log( response );
+          res.json(response);
+          return iotivity.OCStackApplicationResult.OC_STACK_DELETE_TRANSACTION;
+        },
+        null,
+        0 );
+    });
+
+  router.route('/res')
+    .get(function(req, res) {
+      var handle = {};
+
+      iotivity.OCDoResource(
+        handle,
+        iotivity.OCMethod.OC_REST_GET,
+        "/oic/res",
+        null,
+        null,
+        iotivity.OCConnectivityType.OC_ALL,
+        iotivity.OCQualityOfService.OC_HIGH_QOS,
+        function( handle, response ) {
+          console.log( response );
+          res.json(response);
+          return iotivity.OCStackApplicationResult.OC_STACK_DELETE_TRANSACTION;
+        },
+        null,
+        0 );
     });
 
   return router;
