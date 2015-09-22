dc21d16d04c8d912d187f9cc732599284ebf9548
Sakari Poussa <sakari.poussa@intel.com>
2015-09-22 13:44:23 +0300

    Allow cross origin requests

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ app.set('views', './views')
 
 var port = process.env.PORT || 8000;
 
+// Allow cross origin requests
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
