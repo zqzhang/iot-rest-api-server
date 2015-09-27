bc4daaedc2f18a37b8e872e9e4a51dcb104da08f
Sudarsana Nagineni <sudarsana.nagineni@intel.com>
2015-09-27 13:40:27 +0300

    Add support for POST, DELETE, PUT /install/{appId} methods
    This patch contains the implementation of the following methods:
    - POST /install/{appId}: Update single application.
    - DELETE /install/{appId}: Uninstall single installed application.
    - PUT /install/{appId}: Reinstall single installed application.
    
    Signed-off-by: Sudarsana Nagineni <sudarsana.nagineni@intel.com>

--- routes/installRoutes.js
+++ routes/installRoutes.js
@@ -162,13 +162,43 @@ var routes = function(AppFW) {
       sendResponse(res, req.app);
     })
     .post(function(req, res) {
-      res.json(req.app);
-    })
-    .put(function(req, res) {
-      res.json(req.app);
+      var ps_callback = function(err, stdout, stderr) {
+        if (err)
+          res.status(errorStatusCode).send({ error: 'Failed to update the application.'});
+        else
+          res.status(okStatusCode).end();
+      }
+      AppFW.updateApp(req.app[0]["app"], ps_callback);
+
+      res.setTimeout(timeoutValue, function() {
+        res.status(okStatusCode).end();
+      });
     })
     .delete(function(req, res) {
-      res.json(req.app);
+      var ps_callback = function(err, stdout, stderr) {
+        var result;
+
+        if (err)
+          res.status(errorStatusCode).send({ error: 'Failed to uninstall the application.'});
+        else
+          res.status(okStatusCode).end();
+      }
+      AppFW.uninstallApp(req.app[0]["app"], ps_callback);
+
+      res.setTimeout(timeoutValue, function() {
+        res.status(okStatusCode).end();
+      });
+    })
+    .put(function(req, res) {
+      var ps_callback = function(err, stdout, stderr) {
+        if (err && res.finished == false)
+          res.status(errorStatusCode).send({ error: 'Failed to reinstall the application.'});
+      }
+      AppFW.reinstallApp(req.app[0]["app"], ps_callback);
+
+      res.setTimeout(timeoutValue, function() {
+        res.status(okStatusCode).end();
+      });
     });
 
     return router;
